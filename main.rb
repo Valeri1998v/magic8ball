7b9cd25f38900427b988e70b7e1be17dd2990393
Valeria <108891290+Valeri1998v@users.noreply.github.com>
2022-10-11 18:46:10 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -41,6 +41,7 @@ bot.listen do |message|
 case message.text
 when "/start", "/start start"
 	bot.api.send_message(chat_id: message.chat.id
+		text: "Hello, #{message.from.first_name}"
 		)
 	end
 end
