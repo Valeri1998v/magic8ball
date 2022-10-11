554f7034712fb69c6b3a784cdabab2c18e55bdc3
Valeria <108891290+Valeri1998v@users.noreply.github.com>
2022-10-11 18:45:10 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -40,7 +40,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
 bot.listen do |message|
 case message.text
 when "/start", "/start start"
-	bot.api.send_message(
+	bot.api.send_message(chat_id: message.chat.id
 		)
 	end
 end
